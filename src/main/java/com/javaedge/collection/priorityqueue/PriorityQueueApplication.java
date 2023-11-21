54f4ee0265b7c9c8428cd6a5805e7045e5399469
zhr <1219912825@qq.com>
2023-11-21 22:45:20 +0800

    PriorityQueue

---
+++ src/main/java/com/javaedge/collection/priorityqueue/PriorityQueueApplication.java
@@ -0,0 +1,14 @@
+package com.javaedge.collection.priorityqueue;
+
+import java.util.PriorityQueue;
+import java.util.stream.Stream;
+
+public class PriorityQueueApplication {
+    public static void main(String[] args){
+        PriorityQueue<Integer> pq=new PriorityQueue<>((o1, o2) -> o2-o1);//大根堆
+        pq.add(1);
+        pq.add(2);
+        //
+        Stream.of(pq).forEach(System.out:: println);
+    }
+}
