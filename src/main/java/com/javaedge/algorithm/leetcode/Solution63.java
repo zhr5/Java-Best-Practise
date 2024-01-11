fc8aba3ea73cbba9b364bf190523304bc5f1c818
HuirongZhang <1219912825@qq.com>
2024-01-11 19:43:54 +0800

    add 力扣63 不同路径Ⅱ  本地调试

---
+++ src/main/java/com/javaedge/algorithm/leetcode/Solution63.java
@@ -0,0 +1,68 @@
+package com.javaedge.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Solution63 {
+    /*
+    * 答案正确
+    * */
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
+        int m=obstacleGrid.length,n=obstacleGrid[0].length;
+        int [][] dp=new int [m][n];
+        for(int i=0;i<m&&obstacleGrid[i][0]==0;i++){
+            dp[i][0]=1;
+        }
+        for(int i=0;i<n&&obstacleGrid[0][i]==0;i++){
+            dp[0][i]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(obstacleGrid[i][j]==1) continue;
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        System.out.println(Arrays.deepToString(dp));
+        return dp[m-1][n-1];
+    }
+
+
+    /*
+    * 答案错误
+    * */
+    public static int uniquePathsWithObstacles1(int[][] obstacleGrid) {
+
+        int m=obstacleGrid.length,n=obstacleGrid[0].length;
+        int [] [] dp=new int [m][n];//表示机器人到dp[i][j]共存在的路径数
+        //表示机器人到dp[i][j]共存在的路径数
+        //dp[i][j]=dp[i-1][j]+dp[i][j-1]
+        for(int j=0;j<n;j++){
+            if(obstacleGrid[0][j]==0)
+                dp[0][j]=1;
+        }
+        for(int i=0;i<m;i++){
+            if(obstacleGrid[i][0]==0)
+                dp[i][0]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(obstacleGrid[i][j]==0)
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        System.out.println(Arrays.deepToString(dp));
+        return dp[m-1][n-1];
+    }
+
+    public static void main(String args[]){
+        int[][] obstacleGrid =new int [][]{ {0,0},
+                                            {1,1},
+                                            {0,0} };
+        uniquePathsWithObstacles(obstacleGrid);
+        uniquePathsWithObstacles1(obstacleGrid);
+    }
+
+}
