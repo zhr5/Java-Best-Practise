3a74f7b9f141688c5eda94136c21383e55700ef6
HuirongZhang <1219912825@qq.com>
2024-02-07 15:13:47 +0800

    add

---
+++ src/main/java/com/javaedge/algorithm/leetcode/Solution73.java
@@ -0,0 +1,42 @@
+package com.javaedge.algorithm.leetcode;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/**
+ * 矩阵置0，有一行
+ */
+
+class Solution73 {
+    public static void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean flagCol0 = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                flagCol0 = true;
+            }
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if (flagCol0) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+    public  static void main(String args[]){
+        int [] [] matrix=new int [] []{{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+        setZeroes(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+}
+
+
