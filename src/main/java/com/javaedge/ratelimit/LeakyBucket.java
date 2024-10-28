602d5aaf817425a26803991a4640c9b6ea820e04
HuirongZhang <1219912825@qq.com>
2024-10-28 16:10:54 +0800

    add  限流算法实现

---
+++ src/main/java/com/javaedge/ratelimit/LeakyBucket.java
@@ -0,0 +1,24 @@
+package com.javaedge.ratelimit;
+
+public class LeakyBucket {
+    public long timeStamp = System.currentTimeMillis();  // 当前时间
+    public long capacity; // 桶的容量
+    public long rate; // 水漏出的速度
+    public long water; // 当前水量(当前累积请求数)
+
+    public boolean grant() {
+        long now = System.currentTimeMillis();
+        // 先执行漏水，计算剩余水量
+        water = Math.max(0, water - (now - timeStamp) * rate);
+
+        timeStamp = now;
+        if ((water + 1) < capacity) {
+            // 尝试加水,并且水还未满
+            water += 1;
+            return true;
+        } else {
+            // 水满，拒绝加水
+            return false;
+        }
+    }
+}
