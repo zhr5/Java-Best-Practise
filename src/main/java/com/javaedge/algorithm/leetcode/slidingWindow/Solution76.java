c508e7b8d85afff9e290c5a2c3c7927a8dfa4aaa
HuirongZhang <1219912825@qq.com>
2024-11-21 15:08:44 +0800

    add  滑动窗口

--- src/main/java/com/javaedge/algorithm/leetcode/slidingWindow/Solution76.java
+++ src/main/java/com/javaedge/algorithm/leetcode/slidingWindow/Solution76.java
@@ -4,10 +4,37 @@ import org.apache.tools.ant.taskdefs.Tar;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 //最小覆盖子串
 public class Solution76 {
     public String minWindow(String s, String t) {
+        char[] chars = s.toCharArray(), chart = t.toCharArray();
+        int n = chars.length, m = chart.length;
+
+        int[] hash = new int[128];
+        for (char ch : chart) {
+            hash[ch]--;
+        }
+
+        String res = "";
+        for (int i = 0, j = 0, cnt = 0; i < n; i++) {
+            hash[chars[i]]++;
+            if (hash[chars[i]] <= 0) {
+                cnt++;
+            }
+            while (cnt == m && hash[chars[j]] > 0) {// cnt 等于 t 的长度 m 时，说明当前窗口包含了 t 中所有字符
+                hash[chars[j++]]--;
+            }
+            if (cnt == m) {
+                if (res.equals("") || res.length() > i - j + 1) {
+                    res = s.substring(j, i + 1);
+                }
+            }
+        }
+        return res;
+    }
+    public String minWindow1(String s, String t) {
         int m=s.length(),n=t.length();
         Map<Character,Integer> TargetMapCount= new HashMap<>();
         for(char c:t.toCharArray()){
@@ -19,7 +46,7 @@ public class Solution76 {
         while(r<m){
             if(r<m&&TargetMapCount.containsKey(s.charAt(r))){
                 TargetMapCount.put(s.charAt(r),TargetMapCount.get(s.charAt(r))-1);
-                if(TargetMapCount.get(s.charAt(r))==0){//说明匹配完一个字符s.charAt(r),即使字符重复
+                if(TargetMapCount.get(s.charAt(r))<=0){//说明匹配完一个字符s.charAt(r),即使字符重复
                     matched++;
                 }
             }
@@ -46,8 +73,10 @@ public class Solution76 {
     }
 
     public static void main(String[] args) {
-        String s = "ADOBECODEBANC";
-        String t = "ABC";
+/*        String s = "ADOBECODEBANC";
+        String t = "ABC";*/
+        String s = "AA";
+        String t = "AA";
         String res = new Solution76().minWindow(s, t);
         System.out.println(res);
     }
