abb3cce4c75a96d4e29066afd99f69c67fcf9e0a
HuirongZhang <1219912825@qq.com>
2024-11-21 15:14:55 +0800

    add

--- src/main/java/com/javaedge/algorithm/leetcode/slidingWindow/Solution76.java
+++ src/main/java/com/javaedge/algorithm/leetcode/slidingWindow/Solution76.java
@@ -38,14 +38,14 @@ public class Solution76 {
         int m=s.length(),n=t.length();
         Map<Character,Integer> TargetMapCount= new HashMap<>();
         for(char c:t.toCharArray()){
-            TargetMapCount.put(c,TargetMapCount.getOrDefault(c,0)+1);
+            TargetMapCount.put(c,TargetMapCount.getOrDefault(c,0)-1);
         }
         int l=0,r=0,matched=0;
-        int [] resIndex = new int [] {-1,Integer.MAX_VALUE-1};
+        int [] resIndex = new int [] {0,0};
         //先向右移动窗口得到有效子串，再左移收缩窗口优化
         while(r<m){
             if(r<m&&TargetMapCount.containsKey(s.charAt(r))){
-                TargetMapCount.put(s.charAt(r),TargetMapCount.get(s.charAt(r))-1);
+                TargetMapCount.put(s.charAt(r),TargetMapCount.get(s.charAt(r))+1);
                 if(TargetMapCount.get(s.charAt(r))<=0){//说明匹配完一个字符s.charAt(r),即使字符重复
                     matched++;
                 }
@@ -53,13 +53,13 @@ public class Solution76 {
             r++;
             //尝试收缩窗口
             while(matched==n){
-                if(r-l < resIndex[1]-resIndex[0]){
+                if(resIndex[1]-resIndex[0]==0||r-l < resIndex[1]-resIndex[0]){
                     resIndex[0]=l;
                     resIndex[1]=r;//本来应该是r-1，但是后面使用subString方法左闭右开
                 }
 
                 if(l<m&&TargetMapCount.containsKey(s.charAt(l))){
-                    TargetMapCount.put(s.charAt(l), TargetMapCount.get(s.charAt(l))+1);//左移要把已匹配的s.charAt(l)还原
+                    TargetMapCount.put(s.charAt(l), TargetMapCount.get(s.charAt(l))-1);//左移要把已匹配的s.charAt(l)还原
                     if(TargetMapCount.get(s.charAt(l)) > 0){ //s.charAt(l)匹配不完了 试试AABBC  ABC
                         matched--;
                     }
@@ -68,7 +68,7 @@ public class Solution76 {
             }
 
         }
-        return resIndex[1]==Integer.MAX_VALUE-1 ? "" :s.substring(resIndex[0],resIndex[1]);
+        return s.substring(resIndex[0],resIndex[1]);
 
     }
 
@@ -77,7 +77,7 @@ public class Solution76 {
         String t = "ABC";*/
         String s = "AA";
         String t = "AA";
-        String res = new Solution76().minWindow(s, t);
+        String res = new Solution76().minWindow1(s, t);
         System.out.println(res);
     }
 }
