a0721327639632679a89f7073cfc35e321ff16ad
HuirongZhang <1219912825@qq.com>
2024-11-27 18:08:11 +0800

    add

--- src/main/java/com/javaedge/algorithm/leetcode/arrays/Solution42.java
+++ src/main/java/com/javaedge/algorithm/leetcode/arrays/Solution42.java
@@ -23,6 +23,9 @@ public class Solution42 {
         return sum;
     }
 
+    public static int trap1(int[] height) {
+        return 0;
+    }
     public static void main(String[] args) {
         int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
         System.out.println(trap(height));
