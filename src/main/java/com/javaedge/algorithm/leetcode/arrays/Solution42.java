a4fc93fbf132fe19b39ce02939e97976c1062a94
zhr <1219912825@qq.com>
2024-11-28 00:05:20 +0800

    add

--- src/main/java/com/javaedge/algorithm/leetcode/arrays/Solution42.java
+++ src/main/java/com/javaedge/algorithm/leetcode/arrays/Solution42.java
@@ -1,11 +1,16 @@
 package com.javaedge.algorithm.leetcode.arrays;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
 //接雨水
 /*输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
         输出：6
         解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。*/
 public class Solution42 {
     public static int trap(int[] height) {
+        //O(n^2)
         int sum = 0;
         for (int i = 0; i < height.length; i++) {
             if (i == 0 || i == height.length - 1) continue;
@@ -24,8 +29,46 @@ public class Solution42 {
     }
 
     public static int trap1(int[] height) {
-        return 0;
+        //优化到O(n)
+        int sum=0;
+        int [] leftmax=new int[height.length];
+        int [] rightmax=new int[height.length];
+        for(int i=0;i<height.length;i++){
+            leftmax[i]=Math.max(leftmax[i-1],height[i]);
+        }
+        for (int i = height.length-1; i >=0; i--){
+            rightmax[i]=Math.max(rightmax[i+1],height[i]);
+        }
+        for (int i = 0; i < height.length; i++){
+            int count=Math.min(leftmax[i],rightmax[i])-height[i];
+            if(count>0){
+                sum+=count;
+            }
+        }
+
+        return sum;
     }
+    //单调栈 --单调递减
+    public static int trap2(int[] height) {
+        int ans = 0;
+        Deque<Integer> stack = new LinkedList<Integer>();
+        int n = height.length;
+        for (int i = 0; i < n; ++i) {
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                int top = stack.pop();
+                if (stack.isEmpty()) {
+                    break;
+                }
+                int left = stack.peek();
+                int currWidth = i - left - 1;
+                int currHeight = Math.min(height[left], height[i]) - height[top];
+                ans += currWidth * currHeight;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
         System.out.println(trap(height));
