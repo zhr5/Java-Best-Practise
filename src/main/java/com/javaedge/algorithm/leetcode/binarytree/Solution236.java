46f68919beac3984df9b34e653e7714904cbe946
zhr <1219912825@qq.com>
2024-12-04 23:33:41 +0800

    add

--- src/main/java/com/javaedge/algorithm/leetcode/binarytree/Solution236.java
+++ src/main/java/com/javaedge/algorithm/leetcode/binarytree/Solution236.java
@@ -3,6 +3,11 @@ package com.javaedge.algorithm.leetcode.binarytree;
 public class Solution236 {
     //1. p q都能找到 返回最近公共祖先 2. p q 找到一个，返回p q 3. 都没找到 返回null
     //通过递归的方式，逐层向下查找 p 和 q，并根据查找结果来确定最近公共祖先的位置。
+   /* 求最小公共祖先，需要从底向上遍历，那么二叉树，只能通过后序遍历（即：回溯）实现从底向上的遍历方式。
+
+    在回溯的过程中，必然要遍历整棵二叉树，即使已经找到结果了，依然要把其他节点遍历完，因为要使用递归函数的返回值（也就是代码中的left和right）做逻辑判断。
+
+    要理解如果返回值left为空，right不为空为什么要返回right，为什么可以用返回right传给上一层结果。*/
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) {
             return root;
