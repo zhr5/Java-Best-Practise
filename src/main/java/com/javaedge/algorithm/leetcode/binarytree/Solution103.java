478c254d763da1c732c2b830bb47b8c9c1e9bbfc
zhr <1219912825@qq.com>
2024-12-05 21:46:30 +0800

    add

---
+++ src/main/java/com/javaedge/algorithm/leetcode/binarytree/Solution103.java
@@ -0,0 +1,41 @@
+package com.javaedge.algorithm.leetcode.binarytree;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution103 {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root==null) {
+            return res;
+        }
+        Deque<TreeNode> dq = new LinkedList<>();
+        dq.offer(root);
+        int tag = 1;// 表示从左到右
+        while (!dq.isEmpty()) {
+            int len = dq.size();
+            List<Integer> tmp = new LinkedList<>();
+            tag = -tag;
+            while (len-- > 0) {
+                TreeNode node = dq.poll();
+                if(tag<0){
+                    tmp.add(node.val);
+                }else{
+                    tmp.add(0,node.val);// 头插
+                }
+                if (node.left != null) {
+                    dq.offer(node.left);
+                }
+                if (node.right != null) {
+                    dq.offer(node.right);
+                }
+
+            }
+            res.add(new ArrayList<>(tmp));
+        }
+        return res;
+    }
+
+}
