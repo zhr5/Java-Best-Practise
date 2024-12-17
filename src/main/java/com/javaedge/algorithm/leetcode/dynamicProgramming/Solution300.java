51c24d5f997c11e1694d4130bcc5b6ed2785fc7f
zhr <1219912825@qq.com>
2024-12-17 21:54:54 +0800

    add

--- src/main/java/com/javaedge/algorithm/leetcode/dynamicProgramming/Solution300.java
+++ src/main/java/com/javaedge/algorithm/leetcode/dynamicProgramming/Solution300.java
@@ -19,6 +19,23 @@ package com.javaedge.algorithm.leetcode.dynamicProgramming;
         -104 <= nums[i] <= 104*/
 public class Solution300 {
     public int lengthOfLIS(int[] nums) {
-        int [] dp =new int [nums.length];//dp[i]表示
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int max = 1;
+        for (int i = 1; i < n; i++) {
+            int maxVal = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    maxVal = Math.max(maxVal, dp[j]);
+                }
+            }
+            dp[i] = maxVal + 1;
+            max = Math.max(max, dp[i]);
+        }
+        return max; // 最长递增子序列的长度
     }
 }
