2032746015d8dbb1acb59a2aff97260230252437
HuirongZhang <1219912825@qq.com>
2024-12-20 14:43:24 +0800

    add

--- src/main/java/com/javaedge/algorithm/leetcode/stack/Solution84.java
+++ src/main/java/com/javaedge/algorithm/leetcode/stack/Solution84.java
@@ -1,7 +1,34 @@
 package com.javaedge.algorithm.leetcode.stack;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
 //柱形图中的最大矩形
 /*输入：heights = [2,1,5,6,2,3]
         输出：10
         解释：最大的矩形为图中红色区域，面积为 10*/
 public class Solution84 {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(right, n);
+
+        Deque<Integer> mono_stack = new ArrayDeque<Integer>();
+        for (int i = 0; i < n; ++i) {
+            while (!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]) {
+                right[mono_stack.peek()] = i;
+                mono_stack.pop();
+            }
+            left[i] = (mono_stack.isEmpty() ? -1 : mono_stack.peek());
+            mono_stack.push(i);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+    }
 }
