653c3db8529d72bec56e2e8aae402e5e72ebea61
HuirongZhang <1219912825@qq.com>
2024-12-20 14:29:29 +0800

    add

--- src/main/java/com/javaedge/algorithm/leetcode/trick/Solution287.java
+++ src/main/java/com/javaedge/algorithm/leetcode/trick/Solution287.java
@@ -2,6 +2,7 @@ package com.javaedge.algorithm.leetcode.trick;
 
 public class Solution287 {
     public int findDuplicate(int[] nums) {
+        //快慢指针
         int slow = nums[0];
         int fast = nums[nums[0]];
         while (slow!= fast) {
@@ -14,6 +15,25 @@ public class Solution287 {
             fast = nums[fast];
         }
         return slow;
+    }
+    public int findDuplicate1(int[] nums) {
+       //二分查找
+        int l=0,r=nums.length-1;
+        int cnt=0;
+        int res=-1;
+        while(l<=r){
+            int mid=(r-l)/2+l;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]<=mid) cnt++;
+            }
+            if(cnt<=mid){
+                l=mid+1;
+            }else{
+                r=mid-1;
+                res=mid;
+            }
+        }
+        return res;
     }
 
 }
