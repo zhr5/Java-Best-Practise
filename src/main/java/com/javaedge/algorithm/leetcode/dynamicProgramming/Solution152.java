f056927ada8adb3e71ece10d0f5748d40eaa1d2f
zhr <1219912825@qq.com>
2024-12-28 01:06:24 +0800

    add

--- src/main/java/com/javaedge/algorithm/leetcode/dynamicProgramming/Solution152.java
+++ src/main/java/com/javaedge/algorithm/leetcode/dynamicProgramming/Solution152.java
@@ -16,36 +16,35 @@ package com.javaedge.algorithm.leetcode.dynamicProgramming;
         -10 <= nums[i] <= 10
         nums 的任何子数组的乘积都 保证 是一个 32-位 整数*/
 public class Solution152 {
+/*    遍历数组时计算当前最大值，不断更新
+    令imax为当前最大值，则当前最大值为 imax = max(imax * nums[i], nums[i])
+    由于存在负数，那么会导致最大的变最小的，最小的变最大的。因此还需要维护当前最小值imin，imin = min(imin * nums[i], nums[i])
+    当负数出现时则imax与imin进行交换再进行下一步计算
+    时间复杂度：O(n)
+
+    作者：画手大鹏
+    链接：https://leetcode.cn/problems/maximum-product-subarray/solutions/7561/hua-jie-suan-fa-152-cheng-ji-zui-da-zi-xu-lie-by-g/
+    来源：力扣（LeetCode）
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
     public int maxProduct(int[] nums) {
- /*       int [] [] dp =new int [nums.length][nums.length];//dp[i][j]表示【i,j】乘积最大的非空连续 子数组
-
-        for(int i=0;i<nums.length;i++){
-            dp[i][i]=nums[i];
-        }
-
-        for(int i=0;i< nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]*dp[i][j-1]>0){
-                    dp[i][j]=nums[i]*dp[i][j-1];
-                }else if(nums[i]*dp[i][j-1]<0){
-                    if(dp[i][j-1])
-                }
-
+        int max = Integer.MIN_VALUE, imax = 1, imin = 1;// imax 记录以当前元素结尾的最大乘积，imin 记录以当前元素结尾的最小乘积
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] < 0){// 如果当前元素为负数，则交换 imax 和 imin
+                int tmp = imax;
+                imax = imin;
+                imin = tmp;
             }
-        }*/
-        long maxF = nums[0], minF = nums[0];
-        int ans = nums[0];
-        int length = nums.length;
-        for (int i = 1; i < length; ++i) {
-            long mx = maxF, mn = minF;
-            maxF = Math.max(mx * nums[i], Math.max(nums[i], mn * nums[i]));
-            minF = Math.min(mn * nums[i], Math.min(nums[i], mx * nums[i]));
-            if (minF < -1 << 31) {
-                minF = nums[i];
-            }
-            ans = Math.max((int) maxF, ans);
-        }
-        return ans;
+            imax = Math.max(imax*nums[i], nums[i]);
+            imin = Math.min(imin*nums[i], nums[i]);
 
+            max = Math.max(max, imax);
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,3,-2,4};
+        Solution152 solution152 = new Solution152();
+        int maxProduct = solution152.maxProduct(nums);
+        System.out.println(maxProduct);
     }
 }
