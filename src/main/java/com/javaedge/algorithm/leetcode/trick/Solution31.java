1d0e4d856f47dd6e901c19eee315c848226d071d
zhr <1219912825@qq.com>
2025-01-02 00:10:49 +0800

    add

---
+++ src/main/java/com/javaedge/algorithm/leetcode/trick/Solution31.java
@@ -0,0 +1,33 @@
+package com.javaedge.algorithm.leetcode.trick;
+
+public class Solution31 {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = n - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
