a14da8d3af71f932aa2d27afd5350f80afa81612
zhr <1219912825@qq.com>
2025-01-02 22:29:52 +0800

    add

--- src/main/java/com/javaedge/algorithm/leetcode/dynamicProgramming/MinSquares279Plus.java
+++ src/main/java/com/javaedge/algorithm/leetcode/dynamicProgramming/MinSquares279Plus.java
@@ -1,30 +1,33 @@
 package com.javaedge.algorithm.leetcode.dynamicProgramming;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MinSquares279Plus {
     //给你一个整数 n ，返回 和为 n 的完全平方数的最少数量的组成集合 。
     public static List<Integer> findMinSquares(int n) {
         int[] dp = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            dp[i] = Integer.MAX_VALUE;
-        }
+        Arrays.fill(dp, Integer.MAX_VALUE);
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j * j <= i; j++) {
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
         List<Integer> result = new ArrayList<>();
+        // 该代码片段通过动态规划的方法，从给定的数值n中逐步减去平方数，
+        // 直至n变为0，并将每次减去的平方数添加到结果列表result中。
         while (n > 0) {
             for (int j = 1; j * j <= n; j++) {
-                if (dp[n] == dp[n - j * j] + 1) {
+                if (dp[n] == dp[n - j * j] + 1) {// 找到了最少的平方数，将其添加到结果列表中，并将n减去该平方数
+                    // 如果dp[n]等于dp[n - j * j]加1，说明当前的j * j是构成n的其中一个平方数
                     result.add(j * j);
                     n -= j * j;
                     break;
                 }
             }
         }
+
         return result;
     }
 
--- src/main/java/com/javaedge/algorithm/leetcode/trick/Solution31.java
+++ src/main/java/com/javaedge/algorithm/leetcode/trick/Solution31.java
@@ -1,33 +1,45 @@
 package com.javaedge.algorithm.leetcode.trick;
 
+/*整数数组的 下一个排列 是指其整数的下一个字典序更大的排列。更正式地，如果数组的所有排列根据其字典顺序从小到大排列在一个容器中，
+那么数组的 下一个排列 就是在这个有序容器中排在它后面的那个排列。如果不存在下一个更大的排列，那么这个数组必须重排为字典序最小的排列（即，其元素按升序排列）。
+
+        例如，arr = [1,2,3] 的下一个排列是 [1,3,2] 。
+        类似地，arr = [2,3,1] 的下一个排列是 [3,1,2] 。
+        而 arr = [3,2,1] 的下一个排列是 [1,2,3] ，因为 [3,2,1] 不存在一个字典序更大的排列。
+        给你一个整数数组 nums ，找出 nums 的下一个排列。*/
+        // 123465 -> 123546
 public class Solution31 {
     public void nextPermutation(int[] nums) {
+        // 找到下一个排列
         int n = nums.length;
         int i = n - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
+        while (i >= 0 && nums[i] >= nums[i + 1]) {// 找到第一个升序的元素
             i--;
         }
-        if (i >= 0) {
+        if (i >= 0) {// 找到第一个升序的元素后，找到第一个大于nums[i]的元素
             int j = n - 1;
-            while (j >= 0 && nums[j] <= nums[i]) {
+            while (j >= 0 && nums[j] <= nums[i]) {// 找到第一个大于nums[i]的元素
                 j--;
             }
             swap(nums, i, j);
         }
-        reverse(nums, i + 1, n - 1);
+        reverse(nums, i + 1, n - 1);// 反转升序部分 // 反转是为了得到字典序中下一个排列
     }
 
     private void swap(int[] nums, int i, int j) {
+        // 交换数组中的两个元素
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
     private void reverse(int[] nums, int start, int end) {
+        // 反转数组中从start到end的部分
         while (start < end) {
             swap(nums, start, end);
             start++;
             end--;
         }
     }
+
 }
