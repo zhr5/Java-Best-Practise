2a36779bba1e706af3187e607dba10f4d9a8655b
zhr <1219912825@qq.com>
2025-01-09 22:15:02 +0800

    add

--- src/main/java/com/javaedge/algorithm/leetcode/dynamicProgramming/Solution300.java
+++ src/main/java/com/javaedge/algorithm/leetcode/dynamicProgramming/Solution300.java
@@ -1,4 +1,7 @@
 package com.javaedge.algorithm.leetcode.dynamicProgramming;
+
+import java.util.Arrays;
+
 /*给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
         子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。
         例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的 子序列
@@ -18,24 +21,52 @@ package com.javaedge.algorithm.leetcode.dynamicProgramming;
         1 <= nums.length <= 2500
         -104 <= nums[i] <= 104*/
 public class Solution300 {
+    /**
+     * 计算最长递增子序列的长度
+     *
+     * @param nums 输入的整数数组
+     * @return 返回最长递增子序列的长度
+     */
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        if (n == 0) {
-            return 0;
-        }
-        int[] dp = new int[n];
-        dp[0] = 1;
-        int max = 1;
-        for (int i = 1; i < n; i++) {
-            int maxVal = 0;
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    maxVal = Math.max(maxVal, dp[j]);
-                }
+        // 如果输入数组为空，则最长递增子序列长度为0
+        if(nums.length == 0) return 0;
+
+        // 初始化dp数组，dp[i]表示以nums[i]结尾的最长递增子序列的长度
+        int[] dp = new int[nums.length];
+        // 初始化结果变量为0，用于记录整个数组中的最长递增子序列长度
+        int res = 0;
+
+        // 初始化dp数组的所有元素为1，因为最短的递增子序列至少包含一个元素
+        Arrays.fill(dp, 1);
+
+        // 遍历数组，计算每个元素作为结尾的最长递增子序列的长度
+        for(int i = 0; i < nums.length; i++) {
+            // 对于每个元素，再从开头遍历到当前元素之前，寻找比当前元素小的元素
+            for(int j = 0; j < i; j++) {
+                // 如果找到比当前元素小的元素，更新当前元素的最长递增子序列长度
+                if(nums[j] < nums[i])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
             }
-            dp[i] = maxVal + 1;
-            max = Math.max(max, dp[i]);
+            // 更新全局最长递增子序列长度
+            res = Math.max(res, dp[i]);
         }
-        return max; // 最长递增子序列的长度
+        // 返回最长递增子序列的长度
+        return res;
+    }
+
+    public int lengthOfLIS1(int[] nums) {
+        int[] tails = new int[nums.length];
+        int res = 0;
+        for(int num : nums) {
+            int i = 0, j = res;
+            while(i < j) {
+                int m = (i + j) / 2;
+                if(tails[m] < num) i = m + 1;
+                else j = m;
+            }
+            tails[i] = num;
+            if(res == j) res++;
+        }
+        return res;
     }
 }
