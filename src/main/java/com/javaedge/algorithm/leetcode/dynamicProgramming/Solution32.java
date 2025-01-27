36ffafd1591274cb39bc52826cc6e342b93cb6d4
zhr <1219912825@qq.com>
2025-01-27 14:33:29 +0800

    add

--- src/main/java/com/javaedge/algorithm/leetcode/dynamicProgramming/Solution32.java
+++ src/main/java/com/javaedge/algorithm/leetcode/dynamicProgramming/Solution32.java
@@ -89,9 +89,27 @@ public class Solution32 {
         return maxans;
     }
 
+ /*   撇开方法一提及的动态规划方法，相信大多数人对于这题的第一直觉是找到每个可能的子串后判断它的有效性，但这样的时间复杂度会达到 O(n ^ 3)，
+ 无法通过所有测试用例。但是通过栈，我们可以在遍历给定字符串的过程中去判断到目前为止扫描的子串的有效性，同时能得到最长有效括号的长度。
+
+    具体做法是我们始终保持栈底元素为当前已经遍历过的元素中「最后一个没有被匹配的右括号的下标」，这样的做法主要是考虑了边界条件的处理，栈里其他元素维护左括号的下标：
+
+    对于遇到的每个 ‘(’ ，我们将它的下标放入栈中
+    对于遇到的每个 ‘)’ ，我们先弹出栈顶元素表示匹配了当前右括号：
+    如果栈为空，说明当前的右括号为没有被匹配的右括号，我们将其下标放入栈中来更新我们之前提到的「最后一个没有被匹配的右括号的下标」
+    如果栈不为空，当前右括号的下标减去栈顶元素即为「以该右括号为结尾的最长有效括号的长度」
+    我们从前往后遍历字符串并更新答案即可。
+
+    需要注意的是，如果一开始栈为空，第一个字符为左括号的时候我们会将其放入栈中，这样就不满足提及的「最后一个没有被匹配的右括号的下标」，为了保持统一，我们在一开始的时候往栈中放入一个值为 −1 的元素。
+
+    作者：力扣官方题解
+    链接：https://leetcode.cn/problems/longest-valid-parentheses/solutions/314683/zui-chang-you-xiao-gua-hao-by-leetcode-solution/
+    来源：力扣（LeetCode）
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
+
     public static void main(String[] args) {
         Solution32 solution = new Solution32();
-        String s = ")()())";
+        String s = "()))";
         int result = solution.longestValidParentheses1(s);
         System.out.println("The longest valid parentheses substring length is: " + result);
     }
