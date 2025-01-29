72c15975c308460a540dd0161ab1ae61c39a811f
zhr <1219912825@qq.com>
2025-01-29 21:48:07 +0800

    add

---
+++ src/main/java/com/javaedge/algorithm/leetcode/graph/Solution208.java
@@ -0,0 +1,91 @@
+package com.javaedge.algorithm.leetcode.graph;
+
+public class Solution208 {
+    class Trie {
+        /**
+         * Trie树的子节点数组，每个节点最多有26个子节点，对应英文字母a-z
+         */
+        private Trie[] children;
+
+        /**
+         * 表示当前节点是否构成一个完整的单词
+         */
+        private boolean isEnd;
+
+        /**
+         * Trie树节点的构造方法
+         * 初始化子节点数组和结束标志
+         */
+        public Trie() {
+            children = new Trie[26];
+            isEnd = false;
+        }
+
+        /**
+         * 向Trie树中插入一个单词
+         *
+         * @param word 要插入的单词，由小写字母组成
+         */
+        public void insert(String word) {
+            Trie node = this;
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                int index = ch - 'a';
+                if (node.children[index] == null) {
+                    node.children[index] = new Trie();
+                }
+                node = node.children[index];
+            }
+            node.isEnd = true;
+        }
+
+        /**
+         * 在Trie树中搜索一个完整的单词
+         *
+         * @param word 要搜索的单词
+         * @return 如果找到完整的单词则返回true，否则返回false
+         */
+        public boolean search(String word) {
+            Trie node = searchPrefix(word);
+            return node != null && node.isEnd;
+        }
+
+        /**
+         * 搜索Trie树中是否有以给定前缀开头的单词
+         *
+         * @param prefix 要搜索的前缀
+         * @return 如果找到以prefix为前缀的单词则返回true，否则返回false
+         */
+        public boolean startsWith(String prefix) {
+            return searchPrefix(prefix) != null;
+        }
+
+        /**
+         * 辅助函数，用于搜索给定前缀对应的节点
+         *
+         * @param prefix 要搜索的前缀
+         * @return 返回对应前缀的节点，如果前缀不存在则返回null
+         */
+        private Trie searchPrefix(String prefix) {
+            Trie node = this;
+            for (int i = 0; i < prefix.length(); i++) {
+                char ch = prefix.charAt(i);
+                int index = ch - 'a';
+                if (node.children[index] == null) {
+                    return null;
+                }
+                node = node.children[index];
+            }
+            return node;
+        }
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+
---
+++ src/main/java/com/javaedge/algorithm/leetcode/heap/Solution295.java
@@ -0,0 +1,28 @@
+package com.javaedge.algorithm.leetcode.heap;
+
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Solution295 {
+    class MedianFinder {
+        Queue<Integer> minHeap, maxHeap;
+        public MedianFinder() {
+            minHeap = new PriorityQueue<>(); // 小顶堆，保存较大的一半
+            maxHeap = new PriorityQueue<>((x, y) -> (y - x)); // 大顶堆，保存较小的一半
+        }
+        public void addNum(int num) {
+            if (minHeap.size() != maxHeap.size()) {
+                minHeap.add(num);
+                maxHeap.add(minHeap.poll());
+            } else {
+                maxHeap.add(num);
+                minHeap.add(maxHeap.poll());
+            }
+        }
+        public double findMedian() {
+            return minHeap.size() != maxHeap.size() ? minHeap.peek() : (minHeap.peek() + maxHeap.peek()) / 2.0;
+        }
+    }
+
+}
