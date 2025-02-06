33639d0b31676a8ad6261fafb275d6307a71861b
zhr <1219912825@qq.com>
2025-02-06 22:55:48 +0800

    add

--- src/main/java/com/javaedge/algorithm/leetcode/arrays/Solution42.java
+++ src/main/java/com/javaedge/algorithm/leetcode/arrays/Solution42.java
@@ -27,26 +27,50 @@ public class Solution42 {
         return sum;
     }
 
+    /**
+     * 计算给定高度图中可以 trap (接住) 的雨水总量
+     * 该方法使用动态规划优化，将时间复杂度降低到O(n)
+     *
+     * @param height 一个整数数组，其中每个元素代表一根柱子的高度
+     * @return 返回所有柱子可以 trap 的雨水总量
+     */
     public static int trap1(int[] height) {
-        //优化到O(n)
-        int sum = 0;
-        int[] leftmax = new int[height.length];
-        int[] rightmax = new int[height.length];
-        for (int i = 1; i < height.length; i++) {
-            leftmax[i] = Math.max(leftmax[i - 1], height[i]);
-        }
-        for (int i = height.length - 2; i >= 0; i--) {
-            rightmax[i] = Math.max(rightmax[i + 1], height[i]);
-        }
-        for (int i = 0; i < height.length; i++) {
-            int count = Math.min(leftmax[i], rightmax[i]) - height[i];
-            if (count > 0) {
-                sum += count;
-            }
-        }
+    // 优化到O(n)
+    // 总水量
+    int sum = 0;
+    // leftmax[i] 表示从左到右遍历时，到位置i为止的最大高度
+    int[] leftmax = new int[height.length];
+    // rightmax[i] 表示从右到左遍历时，到位置i为止的最大高度
+    int[] rightmax = new int[height.length];
 
-        return sum;
+    // 初始化 leftmax 数组的第一个元素
+    leftmax[0] = height[0];
+    // 从左向右遍历，记录每个位置左侧的最大高度
+    for (int i = 1; i < height.length; i++) {
+        leftmax[i] = Math.max(leftmax[i - 1], height[i]);
     }
+
+    // 初始化 rightmax 数组的最后一个元素
+    rightmax[height.length - 1] = height[height.length - 1];
+    // 从右向左遍历，记录每个位置右侧的最大高度
+    for (int i = height.length - 2; i >= 0; i--) {
+        rightmax[i] = Math.max(rightmax[i + 1], height[i]);
+    }
+
+    // 遍历每个位置，计算该位置可以 trap 的水量，并累加到总水量
+    for (int i = 0; i < height.length; i++) {
+        // 当前位置可以 trap 的水量是由其左右两侧的最大高度中的较小值决定的
+        int count = Math.min(leftmax[i], rightmax[i]) - height[i];
+        // 只有当计算出的水量大于0时，才加入到总水量中
+        if (count > 0) {
+            sum += count;
+        }
+    }
+
+    // 返回总水量
+    return sum;
+}
+
 
     //单调栈 --单调递减
     public static int trap2(int[] height) {
--- src/main/java/com/javaedge/algorithm/leetcode/slidingWindow/Solution438.java
+++ src/main/java/com/javaedge/algorithm/leetcode/slidingWindow/Solution438.java
@@ -7,14 +7,24 @@ import java.util.stream.Collectors;
 
 //找到字符串中的所有字母异位词
 public class Solution438 {
+    /**
+     * 寻找字符串 s 中所有 p 的异位词的起始索引
+     *
+     * @param s 输入的字符串
+     * @param p 指定的异位词
+     * @return 返回所有 p 的异位词的起始索引的列表
+     */
     public List<Integer> findAnagrams(String s,String p){
+        // s 和 p 的长度
         int sLen = s.length();
         int pLen = p.length();
 
+        // 如果 s 的长度小于 p 的长度，直接返回空列表
         if (sLen < pLen) {
             return new ArrayList<>();
         }
 
+        // 存储 p 和 s 中字符的频率
         int[] pCount = new int[26];
         int[] sCount = new int[26];
 
@@ -23,12 +33,14 @@ public class Solution438 {
             pCount[c - 'a']++;
         }
 
+        // 存储结果的列表
         List<Integer> result = new ArrayList<>();
 
-        // 滑动窗口
+        // 滑动窗口的左右指针
         int left = 0;
         int right = 0;
 
+        // 遍历 s，寻找所有异位词
         while (right < sLen) {
             // 扩展窗口
             char currentChar = s.charAt(right);
@@ -49,7 +61,14 @@ public class Solution438 {
 
         return result;
     }
-     // 比较两个数组是否相等
+
+    /**
+     * 比较两个数组是否相等
+     *
+     * @param sCount 第一个数组
+     * @param pCount 第二个数组
+     * @return 如果两个数组相等返回 true，否则返回 false
+     */
     private boolean matches(int[] sCount, int[] pCount) {
         for (int i = 0; i < 26; i++) {
             if (sCount[i] != pCount[i]) {
