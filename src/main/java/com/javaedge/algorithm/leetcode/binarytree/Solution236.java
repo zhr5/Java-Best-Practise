9c33df2dce417701ab8ce6c725559a6263ec3f49
zhr <1219912825@qq.com>
2025-05-07 22:31:21 +0800

    add

--- src/main/java/com/javaedge/algorithm/leetcode/binarytree/Solution236.java
+++ src/main/java/com/javaedge/algorithm/leetcode/binarytree/Solution236.java
@@ -8,18 +8,33 @@ public class Solution236 {
     在回溯的过程中，必然要遍历整棵二叉树，即使已经找到结果了，依然要把其他节点遍历完，因为要使用递归函数的返回值（也就是代码中的left和right）做逻辑判断。
 
     要理解如果返回值left为空，right不为空为什么要返回right，为什么可以用返回right传给上一层结果。*/
+    /**
+     * 寻找二叉树中两个节点的最近公共祖先
+     * 该方法通过递归的方式，逐层向下查找 p 和 q，并根据查找结果来确定最近公共祖先的位置
+     *
+     * @param root 二叉树的根节点
+     * @param p 第一个节点
+     * @param q 第二个节点
+     * @return 最近公共祖先的节点，如果不存在则返回null
+     */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 如果当前节点为空或者是要查找的节点之一，则返回当前节点
         if (root == null || root == p || root == q) {
             return root;
         }
+        // 在左子树中查找 p 和 q
         TreeNode left = lowestCommonAncestor(root.left, p, q);
+        // 在右子树中查找 p 和 q
         TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // 如果左子树中没有找到 p 或 q，则返回右子树中的查找结果
         if (left == null) {
             return right;
         }
+        // 如果右子树中没有找到 p 或 q，则返回左子树中的查找结果
         if (right == null) {
             return left;
         }
+        // 如果 p 和 q 分别在左右子树中找到，则当前节点是最近公共祖先
         return root;//在两侧子树
     }
 
--- src/main/java/com/javaedge/algorithm/leetcode/binarytree/Solution437.java
+++ src/main/java/com/javaedge/algorithm/leetcode/binarytree/Solution437.java
@@ -1,31 +1,45 @@
 package com.javaedge.algorithm.leetcode.binarytree;
 
 //路径总和3
+/**
+ * Solution437类用于解决路径总和的问题
+ * 它提供了一个方法来计算二叉树中所有路径的和等于给定值的路径总数
+ */
 public class Solution437 {
-    /*root = [1,-2,-3,1,3,-2,null,-1]
-    targetSum =-1*/
-    /*    public int pathSum(TreeNode root, int targetSum) {
-            if (root == null) return 0;
-            return mypathSum(root, targetSum) + (root.left == null ? 0 : pathSum(root.left, targetSum))
-                    + (root.right == null ? 0 : pathSum(root.right, targetSum));
-        }
-
-        public int mypathSum(TreeNode root, int targetSum) {
-            if (root == null) return 0;
-            if (root.val == targetSum) return 1;
-            return mypathSum(root.left, targetSum - root.val) + mypathSum(root.right, targetSum - root.val);
-        }*/
+    /**
+     * 计算二叉树中所有路径的和等于给定值的路径总数
+     *
+     * @param root 二叉树的根节点
+     * @param sum 目标和值
+     * @return 路径总数
+     */
     public int pathSum(TreeNode root, long sum) {
-        int res=0;
-        if(root==null) return res;
-        res=dfs(root,sum)+pathSum(root.left,sum)+pathSum(root.right,sum);
+        // 初始化结果变量
+        int res = 0;
+        // 如果根节点为空，直接返回结果
+        if (root == null) return res;
+        // 递归计算当前节点、左子树和右子树的路径总数
+        res = dfs(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
         return res;
     }
-    public int dfs(TreeNode node, long sum){
-        int res=0;
-        if(node==null) return res;
-        if(node.val==sum) res++;
-        res+=dfs(node.left,sum-node.val)+dfs(node.right,sum-node.val);
+
+    /**
+     * 深度优先搜索（DFS）来计算从当前节点开始的路径总数
+     *
+     * @param node 当前节点
+     * @param sum 剩余的和值
+     * @return 从当前节点开始的路径总数
+     */
+    public int dfs(TreeNode node, long sum) {
+        // 初始化结果变量
+        int res = 0;
+        // 如果当前节点为空，直接返回结果
+        if (node == null) return res;
+        // 如果当前节点的值等于剩余的和值，找到一条路径
+        if (node.val == sum) res++;
+        // 递归计算左子树和右子树的路径总数
+        res += dfs(node.left, sum - node.val) + dfs(node.right, sum - node.val);
         return res;
     }
 }
+
